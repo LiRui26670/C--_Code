2abe467ae88a3cbb92135cd66ba3a8da7fe4867e
LiRui26670 <749591639@qq.com>
2026-02-01 11:21:54 +0800

    Create 类做友元.cpp

---
+++ 类和对象/友元/类做友元.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+#include<string>
+
+//类做友元
+
+class House;//类似于函数声明，此处为类声明
+
+//好基友类
+class GoodGay {
+public:
+	GoodGay();
+public:
+	void visit();//访问函数，访问House中的属性
+	House* h;
+};
+
+//家类
+class House {
+	//goodGay是我的好朋友，可以访问我的卧室（私有成员）
+	friend class GoodGay;//写到类的最上面，格式：friend class 类名;
+public:
+	House();
+public:
+	string m_SittingRoom;//客厅
+private:
+	string m_BedRoom;//卧室
+};
+
+//类外写构造函数
+GoodGay::GoodGay()
+{
+	//创建对象
+	h = new House;
+}
+House::House()
+{
+	m_SittingRoom = "客厅";
+	m_BedRoom = "卧室";
+}
+//类外实现成员函数
+void GoodGay::visit()
+{
+	cout << "好基友类正在访问：" << h->m_SittingRoom << endl;
+
+	cout << "好基友类正在访问：" << h->m_BedRoom << endl;
+}
+
+void text01()
+{
+	GoodGay gg;
+	gg.visit();
+}
+int main()
+{
+	text01();
+	
+	return 0;
+}
